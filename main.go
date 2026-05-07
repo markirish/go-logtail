70a6ce1d249294532133b7d9e4cd101cd141ea42
Mark Irish <markdirish@gmail.com>
2026-05-07 12:44:09 -0500

    Basic tailing implemented
    - prints line once newline is encountered
    - can handle multiple files

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
+	"time"
 )
 
 const (
@@ -12,32 +16,120 @@ const (
 	ANSIColorReset = "\033[0m"
 )
 
+type LogEntry struct {
+	File string
+	Text string
+}
+
 func main() {
 	// Set up command-line flags
 	debug := flag.Bool("debug", false, "enable debug logging")
 
 	flag.Parse()
-	files := flag.Args()
+	fileNames := flag.Args()
 
-	for _, file := range files {
+	fileNotFound := false
+
+	// Create the channel early, so that we only have to iterate through the
+	// files once.
+	logEntries := make(chan LogEntry)
+
+	for _, fileName := range fileNames {
 		if *debug {
-			fmt.Println("DEBUG: Checking file:", file)
+			fmt.Println("DEBUG: Checking file:", fileName)
 		}
 
-		_, err := os.Stat(file)
+		fileInfo, err := os.Stat(fileName)
 		if err != nil {
+			fileNotFound = true
 			if os.IsNotExist(err) {
-				fmt.Printf("%sERROR: file not found: %s%s\n", ANSIColorRed, file, ANSIColorReset)
-				continue
+				if *debug {
+					fmt.Printf("DEBUG: %s%s not found%s\n", ANSIColorRed, fileName, ANSIColorReset)
+				}
+			} else {
+				if *debug {
+					fmt.Printf("%sDEBUG: could not stat %s: %v%s\n", ANSIColorRed, fileName, err, ANSIColorReset)
+				}
 			}
-
-			fmt.Printf("%sERROR: could not stat %s: %w%s\n", ANSIColorRed, file, err, ANSIColorReset)
 			continue
 		} else {
 			if *debug {
-				fmt.Printf("DEBUG: %s found\n", file)
+				fmt.Printf("DEBUG: %s found\n", fileName)
 			}
+			// The file exists, so we can start tailing it.
+			file, err := os.Open(fileName)
+			if err != nil {
+				logEntries <- LogEntry{File: fileInfo.Name(), Text: fmt.Sprintf("error opening file: %v\n", err)}
+				continue
+			}
+			go tailFile(file, logEntries)
 		}
 	}
 
+	if fileNotFound {
+		fmt.Printf("%sERROR: one or more files were not found%s\n", ANSIColorRed, ANSIColorReset)
+		os.Exit(1)
+	}
+
+	// Print log entries as they come in.
+	for entry := range logEntries {
+		fmt.Printf("[%s] %s\n", entry.File, entry.Text)
+	}
+}
+
+func tailFile(file *os.File, logEntries chan<- LogEntry) {
+	defer file.Close()
+
+	_, err := file.Seek(0, io.SeekEnd)
+	if err != nil {
+		logEntries <- LogEntry{File: file.Name(), Text: fmt.Sprintf("error seeking file: %v\n", err)}
+		return
+	}
+
+	reader := bufio.NewReader(file)
+
+	// In the case that a line gets split across multiple reads or writes, we
+	// need to keep track of the partial line read until a new line
+	var partial string
+
+	for {
+		// Use ReadString to read until the next newline character, or the EOF if
+		// there is no newline. This way we can handle lines that are written in
+		// multiple parts.
+		line, err := reader.ReadString('\n')
+
+		// Encountered a newline, so we have a complete line to process.
+		if err == nil {
+			text := partial + strings.TrimRight(line, "\n")
+
+			// Reset partial to potnetiall grab the next line if split across writes.
+			partial = ""
+
+			if text != "" {
+				logEntries <- LogEntry{File: file.Name(), Text: text}
+			}
+
+			continue
+		}
+
+		// EOF is treated as a normal condition, even though passed as an error. It
+		// can happen with multiple writes to the file without a newline. Grab the
+		// partial line and store it until the next write, which may complete the
+		// line.
+		if err == io.EOF {
+			if line != "" {
+				partial += line
+			}
+
+			time.Sleep(500 * time.Millisecond)
+			continue
+		}
+
+		// Encountered an error that wasn't EOF! Log it and exit the goroutine.
+		logEntries <- LogEntry{
+			File: file.Name(),
+			Text: fmt.Sprintf("error reading file: %v", err),
+		}
+		return
+	}
 }
